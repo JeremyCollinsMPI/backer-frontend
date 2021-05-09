8d28f6888c95936a52ec7cc593504ab215be9835
Jeremy Collins <jeremycollinsgithub2@gmail.com>
2021-05-09 11:33:43 +0800

    added functions for extracting from url and asking questions

--- index.js
+++ index.js
@@ -83,6 +83,12 @@ class Steps extends React.Component {
   if(this.state.functions[index] == "Entails"){
     return(<div className="l">Hypothesis:<input type="text" id={index} onChange={this.handleAdditionalInputChange}></input></div>)
   }  
+  if(this.state.functions[index] == "Get sentences from url"){
+    return(<div className="l">Url:<input type="text" id={index} onChange={this.handleAdditionalInputChange}></input></div>)
+  }  
+  if(this.state.functions[index] == "Ask question"){
+    return(<div className="l">Question:<input type="text" id={index} onChange={this.handleAdditionalInputChange}></input></div>)
+  }  
   if (example_array.includes(this.state.functions[index])){
     return(<div className="l"><input type="text" id={index} onChange={this.handleAdditionalInputChange}></input></div>)
     } else {
@@ -107,6 +113,7 @@ class Steps extends React.Component {
     <option value="Choose input">Choose input</option>
     <option value="file or directory">Upload file or directory</option>
     <option value="Output 1">Output 1</option>
+    <option value="Output 2">Output 2</option>
   </select>  
        </div>      
        )
@@ -178,6 +185,8 @@ class Steps extends React.Component {
     <option value="Text file to sentences">Text file to sentences</option>
     <option value="Find sentences with string">Find sentences with string</option>
     <option value="Entails">Entails</option>
+    <option value="Get sentences from url">Get sentences from url</option>
+    <option value="Ask question">Ask question</option>
   </select>  
 
          <div className="p">{outputName}</div> 
