b16a5f9b938fd88f77b35dc2dbc9c33834426163
Jeremy Collins <jeremycollinsgithub2@gmail.com>
2021-05-25 12:30:16 +0800

    added make api link option

--- index.js
+++ index.js
@@ -189,6 +189,7 @@ class Steps extends React.Component {
     <option value="Entails">Entails</option>
     <option value="Get sentences from url">Get sentences from url</option>
     <option value="Ask question">Ask question</option>
+    <option value="Make api link">Make api link</option>
   </select>  
 
          <div className="p">{outputName}</div> 
