21c1b9801ea6230cfe2078bf592cfc844d3864ee
Jeremy Collins <jeremycollinsgithub2@gmail.com>
2021-05-27 16:03:36 +0800

    finished api input option

--- index.js
+++ index.js
@@ -54,6 +54,9 @@ class Steps extends React.Component {
       inputs[index]['index'] = inputs[index]['name'].replace('Output ', '');
       inputs[index]['index'] = parseInt(inputs[index]['index']) - 1;
     }    
+    if (inputs[index]['name'] == 'Api input'){
+      inputs[index]['type'] = 'Api input';
+    }
     this.setState({ inputs: inputs });
   }
 
@@ -116,6 +119,7 @@ class Steps extends React.Component {
     <option value="file or directory">Upload file or directory</option>
     <option value="Output 1">Output 1</option>
     <option value="Output 2">Output 2</option>
+    <option value="Api input">Api input</option>
   </select>  
        </div>      
        )
